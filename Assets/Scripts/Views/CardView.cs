33a4c8799785ae148ecbb8a37d9beea00e333429
Summerraim <1625145429@qq.com>
2025-11-08 15:10:31 +0800

    init

---
+++ Assets/Scripts/Views/CardView.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CardView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text title;
+    [SerializeField] private TMP_Text description;
+    [SerializeField] private TMP_Text mana;
+    [SerializeField] private SpriteRenderer imageSR;
+    [SerializeField] private GameObject Wrapper;
+    public Card Card { private set; get; }
+    public void Setup(Card card)
+    {
+        Card = card;
+        title.text = card.Title;
+        description.text = card.Description;
+        mana.text = card.Mana.ToString();
+        imageSR.sprite = card.Image;
+    }
+    void OnMouseEnter()
+    {
+        Wrapper.SetActive(false);
+        Vector3 pos = new(transform.position.x, -2, 0);
+        CardViewHoverSystem.Instance.Show(Card, pos);
+    }
+
+    void OnMouseExit()
+    {
+        CardViewHoverSystem.Instance.Hide();
+        Wrapper.SetActive(true);
+    }
+
+}
